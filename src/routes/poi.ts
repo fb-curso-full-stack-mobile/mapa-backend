617f32f350e52c8860473840288cd64c70e24dc3
Fabio Bergmann <fabiocberg@gmail.com>
2022-02-03 02:37:55 -0300

    feat: backend para projeto do mapa

---
+++ src/routes/poi.ts
@@ -0,0 +1,45 @@
+import { Poi } from "@prisma/client";
+import express from "express";
+import poiController from "../controllers/poi-controller";
+
+const router = express.Router();
+
+router.post("/poi", async (req, res) => {
+  const poi: Poi = req.body;
+  try {
+    const updated = await poiController.save(poi);
+    return res.json({ updated });
+  } catch (e) {
+    console.log("poi post", e);
+    res
+      .status(500)
+      .json({ message: "Não foi possível salvar o ponto de interesse." });
+  }
+});
+
+router.put("/poi", async (req, res) => {
+  const poi: Poi = req.body;
+  try {
+    const updated = await poiController.save(poi);
+    return res.json({ updated });
+  } catch (e) {
+    console.log("poi post", e);
+    res
+      .status(500)
+      .json({ message: "Não foi possível salvar o ponto de interesse." });
+  }
+});
+
+router.get("/poi", async (req, res) => {
+  try {
+    const pois = await poiController.fetch();
+    return res.json({ pois });
+  } catch (e) {
+    console.log("poit get", e);
+    res
+      .status(500)
+      .json({ message: "Não foi possível obter os pontos de interesse." });
+  }
+});
+
+export default router;
